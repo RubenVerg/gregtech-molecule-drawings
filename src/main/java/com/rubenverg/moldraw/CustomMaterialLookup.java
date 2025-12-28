957cd453eb64cc152734df8f7da94a01b6ee52cc
GitHub Action <action@github.com>
2025-12-28 06:44:03 +0000

    style: 自动格式化代码 [skip ci]

--- src/main/java/com/rubenverg/moldraw/CustomMaterialLookup.java
+++ src/main/java/com/rubenverg/moldraw/CustomMaterialLookup.java
@@ -9,8 +9,8 @@ import com.gregtechceu.gtceu.api.data.tag.TagPrefix;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ItemLike;
 
-import java.util.Optional;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * 自定义的 Material 查找器，直接使用 ChemicalHelper 的公共方法。
